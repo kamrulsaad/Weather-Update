961d5df421a91a3c5ed41369cdc37ad2ccb8f0e8
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-03-11 23:13:23 +0600

    Done for Today

--- app/app.js
+++ app/app.js
@@ -2,11 +2,11 @@ const apiKey = '4e3742872a18e370e48236bda95f04eb';
 const inputField = document.getElementById('input-location');
 const searchButton = document.getElementById('search-button');
 
-// inputField.addEventListener("keyup", function(event) {
-//   if (event.key === 'Enter') {
-//      searchButton.click();
-//   }
-// })
+inputField.addEventListener("keyup", function(event) {
+  if (event.key === 'Enter') {
+     searchButton.click();
+  }
+})
 
 searchButton.addEventListener('click', () => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputField.value}&appid=${apiKey}&units=metric`
@@ -17,7 +17,9 @@ searchButton.addEventListener('click', () => {
 
 const displayData = (weather) => {
     console.log(weather);
+    const iconURL = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
     document.getElementById('temperature').innerText = weather.main.temp;
     document.getElementById('weather').innerText = weather.weather[0].main;
     document.getElementById('location').innerText = weather.name;
+    document.getElementById('image').setAttribute('src', iconURL) = weather.name;
 }

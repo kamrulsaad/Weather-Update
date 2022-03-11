a58b8df8d5eae86d40583411679e9881a62979e8
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-03-11 22:57:26 +0600

    Few Works Done

---
+++ app/app.js
@@ -0,0 +1,23 @@
+const apiKey = '4e3742872a18e370e48236bda95f04eb';
+const inputField = document.getElementById('input-location');
+const searchButton = document.getElementById('search-button');
+
+// inputField.addEventListener("keyup", function(event) {
+//   if (event.key === 'Enter') {
+//      searchButton.click();
+//   }
+// })
+
+searchButton.addEventListener('click', () => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputField.value}&appid=${apiKey}&units=metric`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayData(data))
+} )
+
+const displayData = (weather) => {
+    console.log(weather);
+    document.getElementById('temperature').innerText = weather.main.temp;
+    document.getElementById('weather').innerText = weather.weather[0].main;
+    document.getElementById('location').innerText = weather.name;
+}
